6f241330255f43e92a54bdcb74f7d27859250947
halmakey <halmakey@cubicplus.net>
2016-03-01 21:15:17 +0900

    init

--- pdf2png/main.swift
+++ pdf2png/main.swift
@@ -7,6 +7,38 @@
 //
 
 import Foundation
+import AppKit
 
-print("Hello, World!")
+if Process.arguments.count < 2 {
+    print("pdf2png infile outfile width height scale")
+    exit(0)
+}
 
+let inurl = NSURL(fileURLWithPath: Process.arguments[1])
+let outurl = NSURL(fileURLWithPath: Process.arguments[2])
+
+var width: CGFloat = 0, height: CGFloat = 0, scale: CGFloat = 1
+if Process.arguments.count >= 5 {
+    if let n = NSNumberFormatter().numberFromString(Process.arguments[3]) {
+        width = CGFloat(n)
+    }
+    if let n = NSNumberFormatter().numberFromString(Process.arguments[4]) {
+        height = CGFloat(n)
+    }
+}
+
+if Process.arguments.count >= 6 {
+    if let n = NSNumberFormatter().numberFromString(Process.arguments[5]) {
+        scale = CGFloat(n)
+    }
+}
+
+do {
+    try pdf2png(inurl, outurl: outurl, width: width, height: height, scale: scale)
+} catch PDFError.FileNotFound {
+    print("\(inurl.path!) not found.")
+    exit(1)
+} catch PDFError.FileBroken {
+    print("\(outurl.path!) not broken.")
+    exit(2)
+}
